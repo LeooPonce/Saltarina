aea47aeb10331de6efaace7e120e1e29f07d79cd
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-08-30 16:25:24 -0300

    Update Saltarina.gd

--- juego/personaje/Saltarina.gd
+++ juego/personaje/Saltarina.gd
@@ -3,3 +3,9 @@ extends KinematicBody2D
 
 func _ready():
 	print("hola mundo soy Saltarina desde el kinematicbody")
+
+#func _process(delta):
+#	print("hola mundo")
+
+func _physics_process(delta):
+	print("hola mundo")
