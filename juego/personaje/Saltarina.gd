028e2cfe96b8f9f05b2e477c68072b14c2c1e60c
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-08-30 17:22:27 -0300

    update

--- juego/personaje/Saltarina.gd
+++ juego/personaje/Saltarina.gd
@@ -1,11 +1,12 @@
 extends KinematicBody2D
 
-
-func _ready():
-	print("hola mundo soy Saltarina desde el kinematicbody")
-
-#func _process(delta):
-#	print("hola mundo")
+var velocidad = Vector2(150.0, 150.0)
 
 func _physics_process(delta):
-	print("hola mundo")
+	var movimiento = velocidad * tomar_direccion()
+	print(movimiento)
+	move_and_slide(movimiento, Vector2.UP)
+
+func tomar_direccion():
+	var direccion = Vector2(Input.get_action_strength("mov_derecha") - Input.get_action_strength("mov_izquierda"), 1.0)
+	return direccion
