3ac4dd8b2aac5952f5a9c3ca55f1500a78fca377
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-09-06 17:23:01 -0300

    commit

--- juego/personaje/Saltarina.gd
+++ juego/personaje/Saltarina.gd
@@ -1,12 +1,25 @@
 extends KinematicBody2D
 
 export var velocidad = Vector2(150.0, 150.0)
+export var acel_caida = 400
+export var fuerza_salto = 3000
+
+var movimiento = Vector2.ZERO
 
 func _physics_process(delta):
-	var movimiento = velocidad * tomar_direccion()
+	movimiento.x = velocidad.x * tomar_direccion()
+	
+	if not is_on_floor():
+		movimiento.y += acel_caida
+		movimiento.y = clamp(movimiento.y, -fuerza_salto, velocidad.y)
+	
+	if Input.is_action_just_pressed("salto") and is_on_floor():
+		movimiento.y = 0
+		movimiento.y -= fuerza_salto
+	
 	print(movimiento)
 	move_and_slide(movimiento, Vector2.UP)
 
 func tomar_direccion():
-	var direccion = Vector2(Input.get_action_strength("mov_derecha") - Input.get_action_strength("mov_izquierda"), 1.0)
+	var direccion = Input.get_action_strength("mov_derecha") - Input.get_action_strength("mov_izquierda")
 	return direccion
