91234103bc1315b73f14119627717fcbea636966
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-09-06 18:38:45 -0300

    commit

--- juego/personaje/Saltarina.gd
+++ juego/personaje/Saltarina.gd
@@ -7,6 +7,7 @@ export var fuerza_salto = 3000
 var movimiento = Vector2.ZERO
 
 onready var animacion = $Animacion
+onready var audio_salto = $AudioSalto
 
 func _physics_process(delta):
 	movimiento.x = velocidad.x * tomar_direccion()
@@ -38,6 +39,7 @@ func caer():
 
 func saltar():
 	if Input.is_action_just_pressed("salto") and is_on_floor():
+		audio_salto.play()
 		animacion.play("saltar")
 		movimiento.y = 0
 		movimiento.y -= fuerza_salto
