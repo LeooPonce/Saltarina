1a08f1537f0a201ff75a0ce48a1cbf350956507b
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-09-12 22:46:05 -0300

    commit

--- enemigos/Pinchon.gd
+++ enemigos/Pinchon.gd
@@ -7,6 +7,7 @@ var movimiento = Vector2.ZERO
 
 onready var animacion = $AnimatedSprite
 onready var detector_vacio = $DetectorVacio
+onready var detector_pared = $DetectorPared
 
 func _physics_process(delta):
 	caer()
@@ -18,18 +19,28 @@ func caer():
 	if not is_on_floor():
 		movimiento.y = gravedad
 
+
 func caminar():
 	if not animacion.is_playing():
 		animacion.play("caminar")
 	
-	if not detector_vacio.is_colliding():
-		velocidad *= -1
-		detector_vacio.position *= -1
-		animar(animacion.flip_h)
+	detectar_colision()
 	
 	movimiento.x = velocidad
+
+
+func detectar_colision():
+	if not detector_vacio.is_colliding() or detector_pared.is_colliding():
+		velocidad *= -1
+		detector_vacio.position *= -1
+		detector_pared.scale *= -1
+		detector_pared.position.x *= -1
+		animar(animacion.flip_h)
+
 
 func animar(valor_actual):
 	animacion.flip_h = !valor_actual
 
 
+func _on_DetectorJugador_body_entered(body):
+	body.respawn()
