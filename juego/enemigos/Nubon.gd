4ad357df0950fc7cd96bb5ff700bdd7a2b3ac91a
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-09-19 17:57:15 -0300

    commit

--- juego/enemigos/Nubon.gd
+++ juego/enemigos/Nubon.gd
@@ -3,6 +3,7 @@ extends Node2D
 onready var detector_jugador = $Sprite/RayCast2D
 onready var posiciones_disparo = $Sprite/PosicionesDisparo
 onready var cadencia_disparo = $Timer
+onready var sfx_rayos = $Rayos
 
 export var rayo: PackedScene
 
@@ -15,7 +16,8 @@ func _process(delta):
 		cadencia_disparo.start()
 
 func disparar():
-	for posicion in posiciones_disparo.get_children():	
+	sfx_rayos.play()
+	for posicion in posiciones_disparo.get_children():
 		var nuevo_rayo = rayo.instance()
 		nuevo_rayo.crear(posicion.global_position)
 		owner.get_node("Rayos").add_child(nuevo_rayo)
