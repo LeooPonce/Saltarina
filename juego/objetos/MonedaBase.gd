e2ca537cc73ffa5d4524ff4101bc8ec805a27af2
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-09-20 13:49:53 -0300

    commit

--- juego/objetos/MonedaBase.gd
+++ juego/objetos/MonedaBase.gd
@@ -1,6 +1,13 @@
 extends Area2D
 
 onready var animacion := $AnimatedSprite
+onready var animacion_consumir := $AnimationPlayer
+onready var colision_personaje := $ColisionPersonaje
 
 func _ready():
 	animacion.play()
+
+
+func _on_body_entered(body):
+	colision_personaje.set_deferred("disabled", true)
+	animacion_consumir.play("consumir")
