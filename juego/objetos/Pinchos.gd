ccb6c801bc648685189650ebec43c3317ead03e5
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-09-23 19:57:50 -0300

    commit

--- juego/objetos/Pinchos.gd
+++ juego/objetos/Pinchos.gd
@@ -1,7 +1,19 @@
 extends Area2D
 
-var saludo = "Hola mundo soy un "
+export var es_trampa = false
+onready var detector_personaje = $DetectorPersonaje
+var color_trampa = Color.purple
 
+func _ready():
+	if es_trampa:
+		$Sprite.modulate = color_trampa
+		detector_personaje.enabled = true
+		rotation_degrees = 180
+
+func _process(delta):
+	if detector_personaje.is_colliding():
+		detector_personaje.set_deferred("enabled", false)
+		$AnimationPlayer.play("caer")
 
 
 func _on_body_entered(body):
