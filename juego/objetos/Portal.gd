57bc6e78fa08738f64fa08a329f550b371f47e04
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-09-27 14:07:04 -0300

    commit

--- juego/objetos/Portal.gd
+++ juego/objetos/Portal.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 var esta_activado = false
 
+func _input(event):
+	if event.is_action_pressed("pausa"):
+		$AnimationPlayer.play("activado")
 
 func _on_body_entered(body):
 	if esta_activado:
