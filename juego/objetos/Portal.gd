96e44671ba5a4d1ed6ccaf8fa534efe40aa7006a
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-09-27 15:00:13 -0300

    commit

--- juego/objetos/Portal.gd
+++ juego/objetos/Portal.gd
@@ -2,10 +2,14 @@ extends Area2D
 
 var esta_activado = false
 
-func _input(event):
-	if event.is_action_pressed("pausa"):
-		$AnimationPlayer.play("activado")
+#func _ready():
+#	get_parent().connect("abrir_portal", self, "play_animacion")
 
 func _on_body_entered(body):
 	if esta_activado:
 		print("al siguiente nivel")
+
+func play_animacion():
+	esta_activado = true
+	$AnimatedSprite.play("default")
+	$AnimationPlayer.play("activado")
