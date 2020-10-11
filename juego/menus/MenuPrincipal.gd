abc4ec8e7fb9ad7e7e49aef70288bc8fc23ea997
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-10-11 18:44:48 -0300

    commit

--- juego/menus/MenuPrincipal.gd
+++ juego/menus/MenuPrincipal.gd
@@ -2,4 +2,5 @@ extends Control
 
 
 func _on_BotonIniciar_pressed():
-	get_tree().change_scene("res://juego/niveles/Nivel1.tscn")
+	MusicaGlobal.replay()
+	get_tree().change_scene("res://juego/niveles/Nivel1_v2.tscn")
